83166f714110addb51ef4e296acbab2d764b3e88
Hasibur Rahman <hasiburdev@gmail.com>
2023-09-15 16:41:33 +0600

    Add express,prisma

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// prisma.user.create({
+//     data: {
+
+//     }
+// })
+
+console.log("Hello world sdkljlk!");
