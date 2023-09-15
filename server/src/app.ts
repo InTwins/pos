f0e62ba3906419cbc942e333aa6cc0ccf4e8570e
musakazim989 <musakazim989@gmail.com>
2023-09-15 17:30:55 +0600

    add a sample route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express from "express"
 
-export const app = express();
+export const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  res.send("Hello World!")
+})
+
+app.get("/electron", (req, res) => {
+  res.send("Let's explore Electron JS")
+})
