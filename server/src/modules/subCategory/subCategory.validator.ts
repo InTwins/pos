989255d16a749702aa046ba8953acbdbaeebec1d
musakazim989 <musakazim989@gmail.com>
2023-09-24 15:28:00 +0600

    problem fixed

--- server/src/modules/subCategory/subCategory.validator.ts
+++ server/src/modules/subCategory/subCategory.validator.ts
@@ -1,11 +1,11 @@
-import { coerce, z } from "zod"
+import { z } from "zod"
 
 export const createSubCategoryValidator = z.object({
   name: z.coerce.string().min(2, "Enter minimum 2 characters"),
 })
 
 export const updateSubCategoryValidator = z.object({
-  id: coerce.number(),
+  id: z.coerce.number(),
   name: z.string().min(2, "Enter minimum 2 characters"),
 })
 
